0c6cacd3efb5c963a5f2683774929357b5ce4d67
Facundo Castro <facu073@gmail.com>
2026-02-05 21:12:50 -0300

    fix: snackbarhost on scaffold

--- app/src/main/java/com/f1/info/features/racereplay/presentation/ui/screen/RaceReplayScreen.kt
+++ app/src/main/java/com/f1/info/features/racereplay/presentation/ui/screen/RaceReplayScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -92,7 +93,8 @@ fun RaceReplayScreen(viewModel: RaceReplayViewModel = koinViewModel()) {
                     contentDescription = if (state.isPlaying) "Stop" else "Play"
                 )
             }
-        }
+        },
+        snackbarHost = { SnackbarHost(snackbarHostState) }
     )
     { paddingValues ->
         Box(
