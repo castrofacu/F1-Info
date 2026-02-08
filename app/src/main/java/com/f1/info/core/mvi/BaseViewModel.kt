c8728bdb7e034fc34b544993fe99f7771cf4e8b9
Facundo Castro <facu073@gmail.com>
2026-02-08 15:15:15 -0300

    refactor: channel buffered

--- app/src/main/java/com/f1/info/core/mvi/BaseViewModel.kt
+++ app/src/main/java/com/f1/info/core/mvi/BaseViewModel.kt
@@ -14,7 +14,7 @@ abstract class BaseViewModel<State, Intent, Effect>(
     private val _state = MutableStateFlow(initialState)
     val state = _state.asStateFlow()
 
-    private val _effect = Channel<Effect>()
+    private val _effect = Channel<Effect>(Channel.BUFFERED)
     val effect = _effect.receiveAsFlow()
 
     protected fun updateState(reducer: State.() -> State) {
