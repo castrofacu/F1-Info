81d1fc5e1aa7879d322f95f0136344daaf766e57
Facundo Castro <facu073@gmail.com>
2026-02-09 16:59:58 -0300

    feature: added navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     implementation(libs.androidx.compose.icons.extended)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
     implementation(libs.retrofit)
