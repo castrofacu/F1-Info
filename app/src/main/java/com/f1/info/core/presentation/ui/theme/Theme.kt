e6e8b1ed37ff0705a1ec212855d6647edd526161
Facundo Castro <facu073@gmail.com>
2026-02-09 19:35:41 -0300

    feature: light theme

--- app/src/main/java/com/f1/info/core/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/f1/info/core/presentation/ui/theme/Theme.kt
@@ -1,7 +1,9 @@
 package com.f1.info.core.presentation.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val DarkColorScheme = darkColorScheme(
@@ -19,12 +21,34 @@ private val DarkColorScheme = darkColorScheme(
     onError = White
 )
 
+private val LightColorScheme = lightColorScheme(
+    primary = F1Red,
+    secondary = Carbon300,
+    tertiary = Carbon200,
+    background = Carbon50,
+    surface = White,
+    onPrimary = White,
+    onSecondary = Black,
+    onTertiary = Black,
+    onBackground = Carbon900,
+    onSurface = Carbon900,
+    error = Error,
+    onError = White
+)
+
 @Composable
 fun F1InfoTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+
     MaterialTheme(
-        colorScheme = DarkColorScheme,
+        colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
         content = content
