cbb20bb44a1a730dbb202bd1d16ab53da9a25b37
Facundo Castro <facu073@gmail.com>
2026-05-16 17:15:02 -0300

    feature: update shell script for embedding Apple framework and refactor iOS target binaries configuration

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,9 +18,12 @@ kotlin {
 
     jvm()
 
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+    listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach { target ->
+        target.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
 
     sourceSets {
         commonMain.dependencies {
