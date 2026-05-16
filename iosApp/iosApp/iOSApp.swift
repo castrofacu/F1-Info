7e86e175b3d4f4346a1487ff0af2805ad27f2d45
Facundo Castro <facu073@gmail.com>
2026-05-16 17:38:55 -0300

    feature: initialize Koin for dependency injection in iOS app

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
+
+    init() {
+        KoinHelper.start()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
